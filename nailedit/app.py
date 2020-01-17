f2868e80fd9bb7efa518e93b22e7cc7355ad2953
James Bungard <jmbungard@gmail.com>
2020-01-17 19:06:51 +1100

    Toggle pages on and off

--- nailedit/app.py
+++ nailedit/app.py
@@ -1,24 +1,55 @@
-from flask import Flask, render_template
 from datetime import datetime
+from pathlib import Path
+import os
+
+import flask as f
 
 # create the application object
-app = Flask(__name__)
+app = f.Flask(__name__)
+
+def _enabled(round):
+    return os.path.exists(round)
+
+def _enable(round):
+    Path(round).touch()
+
+def _disable(round):
+    os.remove(round)
 
 @app.route('/')
 def welcome():
-    return render_template('home.html')  # render a template
+    return f.render_template('home.html')  # render a template
 
 @app.route('/round1')
 def round1():
-    return render_template('slothpops.html')
+    if (_enabled('round1')):
+        return f.render_template('slothpops.html')
+        
+    return f.render_template('cheating.html')
 
 @app.route('/round2')
 def round2():
-    return render_template('hedgehog.html')
+    if (_enabled('round2')):
+        return f.render_template('hedgehog.html')
+        
+    return f.render_template('cheating.html')
 
-@app.route('/round3')
-def round3():
-    return render_template('cheating.html')
+@app.route('/admin')
+def admin():
+    status = {
+        'round1': _enabled('round1'),
+        'round2': _enabled('round2')
+    }
+    return f.render_template('admin.html', status=status)
+
+@app.route('/toggle/<round>')
+def toggle(round):
+    if _enabled(round):
+        _disable(round)
+    else:
+        _enable(round)
+    
+    return f.redirect(f.url_for('admin'))
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
